bb2e841d2cf6473917800075ed09552eee16d468
viktorKorneev <korneevviktor90@yandex.ru>
2024-09-04 23:04:15 +0300

    step-8

--- main.js
+++ main.js
@@ -1,23 +1,35 @@
 let view = {
-    displayMassage: function(msg) {
-        let messageArea = document.getElementById("messageArea")
-        messageArea.innerHTML = msg
-    },
-    displayHit: function(location) {
-        let cell = document.getElementById(location)
-        cell.setAttribute("class", "hit")
-    },
-    displayMiss: function(location) {
-        let cell = document.getElementById(location)
-        cell.setAttribute("class", "miss")
-    }
-}
+  displayMassage: function (msg) {
+    let messageArea = document.getElementById("messageArea");
+    messageArea.innerHTML = msg;
+  },
+  displayHit: function (location) {
+    let cell = document.getElementById(location);
+    cell.setAttribute("class", "hit");
+  },
+  displayMiss: function (location) {
+    let cell = document.getElementById(location);
+    cell.setAttribute("class", "miss");
+  },
+};
 
-view.displayMiss("00")
-view.displayHit("34")
-view.displayMiss("55")
-view.displayHit("12")
-view.displayMiss("25")
-view.displayHit("26")
+let model = {
+  boardSize: 7,
+  numShips: 3,
+  shipLength: 3,
+  shepsSunk: 0,
+  ships: [
+    { locations: ["06", "16", "26"], hits: ["", "", ""] },
+    { locations: ["24", "34", "44"], hits: ["", "", ""] },
+    { locations: ["10", "11", "12"], hits: ["", "", ""] },
+  ],
+};
 
-view.displayMassage("Tap tap, is this thing on?")
+// view.displayMiss("00")
+// view.displayHit("34")
+// view.displayMiss("55")
+// view.displayHit("12")
+// view.displayMiss("25")
+// view.displayHit("26")
+
+// view.displayMassage("Tap tap, is this thing on?")
