6986451f68ec4e32dcf10593c895ca5d1c7c3c14
viktorKorneev <korneevviktor90@yandex.ru>
2024-09-05 00:42:15 +0300

    step-15

--- main.js
+++ main.js
@@ -53,19 +53,54 @@ let model = {
   },
 };
 
-model.fire("53");
+function parseGuess(guess) {
+  let alphabet = ["A", "B", "C", "D", "E", "F", "G"];
 
-model.fire("06");
-model.fire("16");
-model.fire("26");
+  if (guess === null || guess.length !== 2) {
+    alert("Oops, please enter a letter and a number on the board.");
+  } else {
+    firstChar = guess.charAt(0);
+    let row = alphabet.indexOf(firstChar);
+    let column = guess.charAt(1);
 
-model.fire("34");
-model.fire("24");
-model.fire("44");
+    if (isNaN(row) || isNaN(column)) {
+      alert("Oops, that isn't on the board.");
+    } else if (
+      row < 0 ||
+      row >= model.boardSize ||
+      column < 0 ||
+      column >= model.boardSize
+    ) {
+      alert("Oops, that's off the board!");
+    } else {
+      return row + column;
+    }
+  }
+  return null;
+}
 
-model.fire("12");
-model.fire("11");
-model.fire("10");
+
+console.log(parseGuess("A0"));
+console.log(parseGuess("B6"));
+console.log(parseGuess("G3"));
+console.log(parseGuess("H0"));
+console.log(parseGuess("A7"));
+
+
+
+// model.fire("53");
+
+// model.fire("06");
+// model.fire("16");
+// model.fire("26");
+
+// model.fire("34");
+// model.fire("24");
+// model.fire("44");
+
+// model.fire("12");
+// model.fire("11");
+// model.fire("10");
 
 // view.displayMiss("00")
 // view.displayHit("34")
